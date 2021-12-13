fe489ec6a757385cad8639d6f2ee2c203716db4e
PlayNprovide <playnprovide@gmail.com>
2021-12-14 05:38:59 +0900

    choose a bundler

---
+++ deps.bat
@@ -0,0 +1,4 @@
+call yarn init -y
+call yarn add react react-dom
+call yarn add -D @types/react @types/react-dom typescript @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint eslint-config-airbnb eslint-config-prettier eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-prettier eslint-plugin-react eslint-plugin-react-hooks prettier 
+ 
